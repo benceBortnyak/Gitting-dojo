54c653df073533eeced0933790f3d6cd0df388c2
AndreaSzoboszlai <szobandi@gmail.com>
2018-11-22 09:39:39 +0100

    modified introducion

--- gittie.py
+++ gittie.py
@@ -1,15 +1,14 @@
 def introducion():
 
-	print(Hello, I'm Gittie!")
+	print(Hello, I'm Andi!")
 
-<<<<<<< HEAD
+
 introducion()
 
 def joke():
 	pass
-=======
 
 def add(a, b):
 
 	return(a + b)
->>>>>>> 956d620a7c3993d4b0607a6a8fb1e752f417d969
+
